b4aa48f92400a76d31d4a5845c1263e9be6a00b6
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-22 12:50:44 +0200

    feat: restore commit after the seplunking

--- index.ts
+++ index.ts
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 
 import aggregateCommits from "./src/aggregateCommits";
-import checkoutAndExec from "./src/checkoutAndExec";
+import { GitRepository } from "./src/gitRepository";
 import { listCommits } from "./src/listCommits";
 import parseConfig from "./src/parseConfig";
 import selectCommitsFromAgg from "./src/selectCommitsFromAgg";
 import { AggregatedCommits } from "./src/types/aggregatedCommits.type";
+import { Commit } from "./src/types/commit.type";
 
 const config = parseConfig();
 const commits = listCommits(config.repo, config.maxCommits);
 const aggregatedCommits: AggregatedCommits = aggregateCommits(commits, "day"); // TODO aggregation type
 const selectedCommits = selectCommitsFromAgg(aggregatedCommits); // TODO oldest or newest
 
-console.log("Aggregated commits:", aggregatedCommits);
-console.log("Selected commits:", selectedCommits);
-commits.forEach((commit) => {
-  checkoutAndExec(commit.hash, config.command);
-});
+function spelunkRepository(commits: Commit[]) {
+  const gitRepository = new GitRepository();
+  gitRepository.getInitialCommitHash();
+
+  commits.forEach((commit) => {
+    gitRepository.checkoutAndExec(commit.hash, config.command);
+  });
+
+  gitRepository.restoreInitialCommit();
+}
+
+spelunkRepository(selectedCommits);
