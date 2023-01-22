c9bd0e4525f82b5bf12411773a90b063d525aeee
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-22 13:42:45 +0200

    refc: make index more readable

--- index.ts
+++ index.ts
@@ -6,23 +6,21 @@ import { listCommits } from "./src/listCommits";
 import parseConfig from "./src/parseConfig";
 import selectCommitsFromAgg from "./src/selectCommitsFromAgg";
 import { AggregatedCommits } from "./src/types/aggregatedCommits.type";
-import { Commit } from "./src/types/commit.type";
 
-const config = parseConfig();
-const commits = listCommits(config.repo, config.maxCommits);
-const aggregatedCommits: AggregatedCommits = aggregateCommits(commits, "day"); // TODO aggregation type
-const selectedCommits = selectCommitsFromAgg(aggregatedCommits); // TODO oldest or newest
+function spelunkRepository() {
+  const config = parseConfig();
+  const commits = listCommits(config.repo, config.maxCommits);
+  const aggregatedCommits: AggregatedCommits = aggregateCommits(commits, "day"); // TODO aggregation type
+  const selectedCommits = selectCommitsFromAgg(aggregatedCommits); // TODO oldest or newest
 
-function spelunkRepository(commits: Commit[]) {
   const gitRepository = new GitRepository();
-  const initialCommitHash = gitRepository.getInitialState();
-  console.log(`Initial commit hash: ${initialCommitHash}`);
+  gitRepository.getInitialState();
 
-  commits.forEach((commit) => {
+  selectedCommits.forEach((commit) => {
     gitRepository.checkoutAndExec(commit.hash, config.command);
   });
 
   gitRepository.restoreInitialState();
 }
 
-spelunkRepository(selectedCommits);
+spelunkRepository();
