39d8dbf636ffd28594415c07bbbdea39d77fcf52
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-23 19:53:52 +0200

    feat: adjust tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -8,11 +8,11 @@ describe("groupBy", () => {
     console.log(output.toString());
     const outputObject = JSON.parse(output.toString());
     expect(outputObject).toBeDefined();
-    expect(outputObject[0].output).toEqual(8);
+    expect(parseInt(outputObject[0].output)).toEqual(8);
     expect(outputObject[0].commit).toEqual(
       "7237c53d04306944ac7f70957f961216df3f5446"
     );
-    expect(outputObject[1].output).toEqual(16);
+    expect(parseInt(outputObject[1].output)).toEqual(16);
     expect(outputObject[1].commit).toEqual(
       "8661f6fff4b743d8d9603690b796e278c463b3b6"
     );
@@ -26,6 +26,6 @@ describe("groupBy", () => {
     console.log(output.toString());
     const outputObject = JSON.parse(output.toString());
     expect(outputObject).toBeDefined();
-    expect(outputObject[0].output > 15).toBeTruthy();
+    expect(parseInt(outputObject[0].output) > 15).toBeTruthy();
   });
 });
