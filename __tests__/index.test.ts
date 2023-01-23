7693266382837022c233fb10badc041c453c71ee
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-23 20:04:04 +0200

    fix: remove unneeded test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -26,13 +26,4 @@ describe("groupBy", () => {
     expect(outputObject).toBeDefined();
     expect(parseInt(outputObject[0].output) > 15).toBeTruthy();
   });
-
-  it("should support month aggregation and max commits", async () => {
-    const output = execSync(
-      'AGG=month MAX_COMMITS=1 node ./dist/index.js "git ls-files | wc -l"'
-    );
-    const outputObject = JSON.parse(output.toString());
-    expect(outputObject).toBeDefined();
-    expect(parseInt(outputObject[0].output) > 22).toBeTruthy();
-  });
 });
