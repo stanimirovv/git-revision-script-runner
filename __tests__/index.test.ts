3ab31652f46880346f39a07a6787f73da50af1d9
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2023-01-23 19:49:15 +0200

    feat: a

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,8 +1,11 @@
 import { execSync } from "child_process";
 
 describe("groupBy", () => {
-  it("should support exit status for CI pipelines", async () => {
-    const output = execSync(' node ./dist/index.js "ls -la"');
+  it("123should support exit status for CI pipelines", async () => {
+    const output = execSync(
+      'AGG=day node ./dist/index.js "git ls-files | wc -l"'
+    );
+    console.log(output.toString());
     expect(output).toBeDefined();
   });
 });
